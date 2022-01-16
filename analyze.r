fe49a782aaf863d5cc1654e58ceb3382d10ac4db
Biby99 <94541261+Biby99@users.noreply.github.com>
2022-01-16 22:28:14 +0100

    EDIT grafici colorati con valori relativi

--- analyze.r
+++ analyze.r
@@ -1,67 +1,84 @@
+
 library(tidyverse)
 library(RColorBrewer) # color palettes
 
 
-workers_data <- read.csv("result/SocComP2/Dataframe/workers_data.csv")
+workers_data <- read.csv("workers_data.csv")
 
 
 questionary <- workers_data %>%
   filter(worker_paid == "True") %>%
   filter(platform == "mturk") %>%
   select(starts_with(c("q_0_", "worker_id"))) %>%
-  distinct
+  distinct 
 
-ggplot(questionary, aes(q_0_0_answer_text, fill=q_0_0_answer_text, y = ..prop.., group = 1)) +
-  geom_bar(stat="count") +
+questionary2 <- aggregate(q_0_0_required ~ q_0_0_answer_text, data = questionary, FUN = sum)
+questionary2$q_0_0_answer_text_percent <- round((questionary2$q_0_0_required/sum(questionary2$q_0_0_required))*100, 2)
+
+ggplot(questionary2 %>% mutate(TC=paste(q_0_0_answer_text_percent, suffix ="%" )),
+       aes(x=q_0_0_answer_text, y=q_0_0_answer_text_percent, fill=q_0_0_answer_text)) +
+  geom_bar(stat="identity")+
   scale_fill_brewer(palette="Set2") +
-  geom_text(stat='count', aes(label=..prop..), vjust=1.6, color="white", size=3.5)+
+ geom_text(aes(label=TC), vjust=1.6, color="black", size=3.5)+
   labs(
     x = "What is your main use on Twitter?",
     y = "Relative frequency",
   ) +
   scale_x_discrete(limits = c("Entertainment", "Politics", "Current news", "Other things")) +
-  theme_minimal()
+  theme_minimal()+ theme(legend.title	=element_blank()) 
 
-ggplot(questionary, aes(q_0_0_answer_text, fill=q_0_0_answer_text)) +
-  geom_bar(stat="count") +
+
+questionary2 <- aggregate(q_0_1_required ~ q_0_1_answer_text, data = questionary, FUN = sum)
+questionary2$q_0_1_answer_text_percent <- round((questionary2$q_0_1_required/sum(questionary2$q_0_1_required))*100, 2)
+
+ggplot(questionary2 %>% mutate(TC=paste(q_0_1_answer_text_percent, suffix ="%" )),
+       aes(x=q_0_1_answer_text, y=q_0_1_answer_text_percent, fill=q_0_1_answer_text)) +
+  geom_bar(stat="identity")+
   scale_fill_brewer(palette="Set2") +
-  geom_text(stat='count', aes(label=..count..), vjust=1.6, color="black", size=3.5)+
-  labs(
-    x = "What is your main use on Twitter?",
-    y = "Relative frequency",
-    fill = NULL
-  ) +
-  scale_x_discrete(limits = c("Entertainment", "Politics", "Current news", "Other things")) +
-  theme_minimal()
-
-
-questionary %>%
-  ggplot(aes(q_0_1_answer_text, fill=q_0_1_answer_text, y = ..prop.., group = 1)) +
-  geom_bar(stat="count") +
-  scale_fill_brewer(palette="Set2") +
-  geom_text(stat='count', aes(label=..prop..), vjust=1.6, color="white", size=3.5)+
+ geom_text(aes(label=TC), vjust=1.6, color="black", size=3.5)+
   labs(
     x = "How long do you spend on Twitter every day?",
     y = "Relative frequency",
   ) +
   scale_x_discrete(limits = c("Less then 5 min", "Between 10 or 20 min", "More then half an hour")) +
-  theme_minimal()
+  theme_minimal()+ theme(legend.title	=element_blank()) 
 
-ggplot(questionary, aes(q_0_2_answer_text, fill=q_0_2_answer_text, y = ..prop.., group = 1)) +
-  geom_bar(stat="count") +
-  geom_text(stat='count', aes(label=..prop..), vjust=1.6, color="white", size=3.5)+
+
+
+
+questionary2 <- aggregate(q_0_2_required ~ q_0_2_answer_text, data = questionary, FUN = sum)
+questionary2$q_0_2_answer_text_percent <- round((questionary2$q_0_2_required/sum(questionary2$q_0_2_required))*100, 2)
+
+ggplot(questionary2 %>% mutate(TC=paste(q_0_2_answer_text_percent, suffix ="%" )),
+       aes(x=q_0_2_answer_text, y=q_0_2_answer_text_percent, fill=q_0_2_answer_text)) +
+  geom_bar(stat="identity")+
+  scale_fill_brewer(palette="Set2") +
+ geom_text(aes(label=TC), vjust=1.6, color="black", size=3.5)+
   labs(
     x = "How often do you have an active interaction with tweet or retweet?",
     y = "Relative frequency",
   ) +
   scale_x_discrete(limits = c("Every day", "Twice a week", "Every week", "I don't have any")) +
-  theme_minimal()
+  theme_minimal()+ theme(legend.title	=element_blank()) 
 
-ggplot(questionary, aes(fct_rev(q_0_3_answer_text), fill=q_0_3_answer_text, y = ..prop.., group = 1)) +
-  geom_bar(stat="count") +
-  geom_text(stat='count', aes(label=..prop..), vjust=1.6, color="white", size=3.5)+
+
+
+
+
+questionary2 <- aggregate(q_0_3_required ~ q_0_3_answer_text, data = questionary, FUN = sum)
+questionary2$q_0_3_answer_text_percent <- round((questionary2$q_0_3_required/sum(questionary2$q_0_3_required))*100, 2)
+
+ggplot(questionary2 %>% mutate(TC=paste(q_0_3_answer_text_percent, suffix ="%" )),
+       aes(x=q_0_3_answer_text, y=q_0_3_answer_text_percent, fill=q_0_3_answer_text)) +
+  geom_bar(stat="identity")+
+  scale_fill_brewer(palette="Set2") +
+ geom_text(aes(label=TC), vjust=1.6, color="black", size=3.5)+
   labs(
     x = "Have you ever bought something that you have seen it on Twitter?",
     y = "Relative frequency",
   ) +
-  theme_minimal()
+  scale_x_discrete(limits = c("Yes", "No")) +
+  theme_minimal()+ theme(legend.title	=element_blank()) 
+
+
+
